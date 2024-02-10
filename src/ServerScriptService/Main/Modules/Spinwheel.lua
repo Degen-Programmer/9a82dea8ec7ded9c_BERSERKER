6b3b3759d6aa785b92b3e47ef68cf63d3799c3da
MunishShinde <munishshinde69@gmail.com>
2024-02-10 17:12:15 +0530

    Rewrite Of spinwheel.server.lua

--- src/ServerScriptService/Main/Modules/Spinwheel.lua
+++ src/ServerScriptService/Main/Modules/Spinwheel.lua
@@ -1,6 +1,76 @@
 local MarketplaceService = game:GetService("MarketplaceService")
 local Spinwheel = {}
-Spinwheel.Debounces = {}
+Spinwheel.Sessions = {}
+Spinwheel.Chances = {
+
+    ["1000 Coins"] = {
+
+        Chance = 10;
+        RewardType = "Currency";
+        Reward = 1000;
+
+    };
+
+    ["500 Coins"] = {
+
+        Chance = 20;
+        RewardType = "Currency";
+        Reward = 500;
+
+    };
+
+    ["Sword"] = {
+
+        Chance = 30;
+        RewardType = "Item";
+        Reward = "WoodenSword";
+        Container = "Weapons";
+
+    };
+
+    ["300 Coins"] = {
+
+        Chance = 40;
+        RewardType = "Currency";
+        Reward = 300;
+
+    };
+
+    ["1 Coin"] = {
+
+        Chance = 50;
+        RewardType = "Currency";
+        Reward = 1;
+
+    };
+
+    ["Other Sword"] = {
+
+        Chance = 20;
+        RewardType = "Item";
+        Reward = "Endsword";
+        Container = "Weapons"
+
+    };
+
+    ["Aura"] = {
+
+        Chance = 60;
+        RewardType = "Item";
+        Reward = "CorpsePiler";
+        Container = "Auras";
+
+    };
+
+    ["Dash"] = {
+
+        Chance = 10;
+        RewardType = "Item";
+        Reward = "Dash";
+        Container = "Abilities";
+
+    };
+}
 
 local dataMain = require(game.ServerScriptService.Main.Data)
 local combat = require(game.ServerScriptService.Main.Combat.Users);
@@ -11,265 +81,8 @@ local MY_FUCKING_ROBUX = require(script.Parent.Products);
 
 local ClientCommunication = net.ReferenceBridge("ClientCommunication");
 
-Spinwheel.Chances = {
-
-    ["500 Coins"] = {
-
-        Chance = 50;
-        RewardType = "Currency";
-        Index = 3;
-
-        Arguments = {
-            
-            Currency = "Currency";
-            Reward = 500;
-
-        }
-
-    }; -- // 1st Index.
-
-    ["Endsword"] = {
-
-        Chance = 1;
-        RewardType = "Item";
-        Index = 4;
-
-        Arguments = {
-            
-            Container = "Weapons";
-            Item = "Endsword";
-
-        }
-
-    }; -- // 2nd Index.
-
-    ["TheReaping"] = {
-
-        Chance = 10;
-        RewardType = "Item";
-        Index = 5;
-
-        Arguments = {
-            
-            Container = "Abilities";
-            Item = "TheReaping";
-
-        }
-
-    }; -- // 3rd Index.
-
-    ["1000 Coins"] = {
-
-        Chance = 40;
-        RewardType = "Currency";
-        Index = 6;
-        Arguments = {
-            
-            Currency = "Currency";
-            Reward = 1000;
-
-        }
-
-    }; -- // 4th Index.
-
-    ["1500 Coins"] = {
-
-        Chance = 30;
-        RewardType = "Currency";
-
-        Arguments = {
-            
-            Currency = "Currency";
-            Reward = 1500;
-
-        };
-
-        Index = 7;
-
-    }; -- // 5th Index.
-
-    ["Dodge Roll"] = {
-
-        Chance = 50;
-        RewardType = "Item";
-        Index = 8;
-
-        Arguments = {
-            
-            Container = "Abilities";
-            Item = "TheReaping";
-
-        }
-
-    }; -- // 6th Index.
-
-    ["2000 Coins"] = {
-
-        Chance = 20;
-        RewardType = "Currency";
-        Index = 1;
-
-        Arguments = {
-            
-            Currency = "Currency";
-            Reward = 1500;
-
-        };
-
-    }; -- // 6th Index.
-
-    ["Wooden Sword"] = {
-
-        Chance = 10;
-        RewardType = "Item";
-        Index = 2;
-
-        Arguments = {
-            
-            Container = "Weapons";
-            Item = "WoodenSword";
-
-        }
-
-    }; -- // 7th Index.
+function Spinwheel.ChooseItem()
     
-
-}
-
-function Spinwheel.ProcessTransaction(Player : Player, Kwargs : {})
-
-    print("PRocessing transacxtion...")
-    
-    local ProductName = Kwargs.Product;
-    local ProductID;
-
-    for P_ID, Array : {} in pairs(MY_FUCKING_ROBUX.Spins) do
-        if Array.Name == ProductName then
-            ProductID = tonumber(P_ID)
-        end 
-    end
-
-    print(ProductID);
-
-    if ProductID then
-        local approved, rejected = pcall(function()
-            return MarketplaceService:PromptProductPurchase(Player, ProductID)
-        end)
-    end
-
-    task.spawn(function()
-        MarketplaceService.PromptProductPurchaseFinished:Connect(function(userId, productId, isPurchased)
-            if isPurchased == true then
-                Spinwheel.UpdateSpins(Player, dataMain:Get(Player).Data.Spins)
-            end
-        end)
-    end)
-end
-
-function Spinwheel.Process(Player : Player, Kwargs : {})
-
-    print("Processing..")
-
-    if not Spinwheel.Debounces[Player.UserId] then
-        Spinwheel.Debounces[Player.UserId] = false
-    end
-
-    local playerData = dataMain:Get(Player).Data;
-    local Spins = playerData.Spins;
-
-    if Spinwheel.Debounces[Player.UserId] == false then
-        Spinwheel.Debounces[Player.UserId] = true
-
-        if Spins == 0 then
-
-            Spinwheel.Reject(Player)
-
-            task.delay(1, function()
-                Spinwheel.Debounces[Player.UserId] = false
-            end)
-        else
-    
-            print("Can Spin..")
-    
-            playerData.Spins = playerData.Spins - 1
-
-            print(playerData.Spins)
-    
-            Spinwheel.UpdateSpins(Player, playerData.Spins)
-            local result, array = Spinwheel.Spin()
-            local index = array.Index
-    
-            ClientCommunication:Fire(net.Players({Player}), {
-    
-                Request = "Spinwheel";
-                Action = "Spin";
-        
-                Arguments = {Index = index, Item = result}
-        
-            })
-
-            task.delay(5, function()
-
-                Spinwheel.Debounces[Player.UserId] = false
-
-                if array.RewardType == "Currency" then
-
-                    local currency = array.Arguments.Currency;
-                    local reward = array.Arguments.Reward;
-
-                    print(playerData[currency])
-
-                    playerData[currency] = playerData[currency] + reward;
-
-                    print(playerData[currency])
-
-                    ClientCommunication:Fire(net.Players({Player}), {
-	
-                        Request = "Inventory";
-                        Action = "LoadCash";
-                
-                        Arguments = {
-                            Cash = playerData[currency];
-                        }
-                    })
-
-                    print("Rewarded player.")
-                    
-                end
-            end)
-        end
-    else
-        Spinwheel.Reject(Player)
-        print("IS ALR SPINNING.")
-    end
-end
-
-function Spinwheel.Reject(Player)
-    ClientCommunication:Fire(net.Players({Player}), {
-
-        Request = "Spinwheel";
-        Action = "Reject";
-
-        Arguments = {}
-
-    })
-end
-
-function Spinwheel.UpdateSpins(Player, newSpinAmount)
-
-    ClientCommunication:Fire(net.Players({Player}), {
-
-        Request = "Spinwheel";
-        Action = "UpdateSpins";
-
-        Arguments = {
-            Spins = newSpinAmount;
-        }
-
-    })
-end
-
-function Spinwheel.Spin()
     local Rarities = Spinwheel.Chances;
 
     local function GetItem()
@@ -292,6 +105,19 @@ function Spinwheel.Spin()
     end
 
     return GetItem()
+
+end
+
+function Spinwheel.ProcessRequest(player : Player, Kwargs)
+    
+    if Spinwheel.Sessions[player.UserId] then return end
+    Spinwheel.Sessions[player.UserId] = "X";
+
+    local player_data = dataMain:Get(player).Data;
+    if player_data.Spins == 0 then return end
+
+    print("Can spin", Spinwheel.ChooseItem())
+
 end
 
 return Spinwheel
