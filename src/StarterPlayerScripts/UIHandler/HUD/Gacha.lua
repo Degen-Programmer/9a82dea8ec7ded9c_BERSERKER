d51ff6d092aeedab3f2550f94bb8b2bbf4359624
MunishShinde <munishshinde69@gmail.com>
2024-02-12 19:41:55 +0530

    Add opening functionality.

--- src/StarterPlayerScripts/UIHandler/HUD/Gacha.lua
+++ src/StarterPlayerScripts/UIHandler/HUD/Gacha.lua
@@ -47,7 +47,7 @@ local Gacha = {}
 Gacha.__index = Gacha
 
 local Playergui = game.Players.LocalPlayer.PlayerGui
-local CardPacks = workspace.Maps.CardPacks;
+local CardPacks = workspace.Map.CardPacks;
 
 local Camera : Camera? = workspace.CurrentCamera;
 local Tweenservice = game:GetService("TweenService");
@@ -64,6 +64,8 @@ function Gacha.New()
 
     self._name = "Gacha";
     self._container = CardPacks
+    self._elements = {}
+
 	return setmetatable(self, Gacha)
 
 end
@@ -72,15 +74,62 @@ function Gacha:SetAdornee()
     
 end
 
+function Gacha:_init_runner_thread()
+
+    local bounds = Vector2.new(-0.5, 0.5)
+	self:SetAdornee()
+
+	self._runnerThread = task.spawn(function()
+		game:GetService("RunService").RenderStepped:Connect(function()
+
+			local screenSize = Camera.ViewportSize;
+			local mousePos = (UIS:GetMouseLocation() - screenSize / 2) * (2 / screenSize)
+
+			local yaw = CFrame.fromEulerAnglesXYZ(
+
+				math.rad(mousePos.Y * bounds.Y),
+				math.rad(mousePos.X * bounds.X),
+				0
+
+			)
+			
+			self._element.CFrame = Camera.CFrame * self._element._OFFSET.Value * yaw;
+			
+		end)
+	end)
+end
+
+function Gacha:Open()
+
+    self:_init_runner_thread()
+    Tweenservice:Create(self._element, TweenInfo.new(.25), {Size = Vector3.new(0.379, 0.532, 0.001)}):Play()
+
+end
+
 function Gacha:Deploy()
 
-    for _, part : Part in ipairs(self._container) do
+    local element : Part = game.ReplicatedStorage.UI.Card;
+	local newElement = element:Clone()
+	newElement.Parent = workspace;
+
+    self._elements[1] = element;
+    self._element = self._elements[1]
+
+    for _, part : Part in ipairs(self._container:GetChildren()) do
         
         local Activator : ProximityPrompt = part:FindFirstChild("Activator")
         Activator.UIOffset = Vector2.new(10000000001, 10000000001)
 
         Activator.Triggered:Connect(function(playerWhoTriggered)
-            print("Uwu") 
+            self:Open()
+        end)
+
+        Activator.PromptShown:Connect(function(playerWhoTriggered)
+            Tweenservice:Create(part._R, TweenInfo.new(0.15), {Size = UDim2.new(6, 0, 7, 0)}):Play() 
+        end)
+
+        Activator.PromptHidden:Connect(function(playerWhoTriggered)
+            Tweenservice:Create(part._R, TweenInfo.new(0.15), {Size = UDim2.new(0, 0, 0, 0)}):Play()
         end)
     end
 
